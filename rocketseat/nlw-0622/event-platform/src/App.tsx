c59904184a5a1ecd69790f92be694c35c8ef228d
Bruno Galvão <bruno_galvao@outlook.com>
2022-06-21 17:06:02 -0300

    adicionando o tailwind

--- rocketseat/nlw-0622/event-platform/src/App.tsx
+++ rocketseat/nlw-0622/event-platform/src/App.tsx
@@ -1,7 +1,19 @@
 function App() {
+
+  let texto = "Titulo";
   return (
-    
-    <h1 className="text-5xl text-center text-violet-400">Testando algumas coisas</h1>
+
+    <div className="grid grid-cols-2 gap-4 place-content-around mx-8">
+      <div>
+        <h1 className="text-5xl text-center font-thin uppercase text-violet-400 m-6">{texto}</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi officiis consequatur dolores hic incidunt earum esse ipsum animi ea provident, voluptatum aliquam. A, quibusdam. Facere nihil aliquam beatae est natus.</p>
+      </div>
+
+      <div>
+        <h1 className="title">{texto}</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi officiis consequatur dolores hic incidunt earum esse ipsum animi ea provident, voluptatum aliquam. A, quibusdam. Facere nihil aliquam beatae est natus.</p>
+      </div>
+    </div>
 
   )
 }
