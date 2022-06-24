7b09c9277347c0d86892fe278b1f0d3cdfd9387a
Bruno Galvão <bruno_galvao@outlook.com>
2022-06-24 09:46:15 -0300

    atualizacao

--- _personalPage/personalpage/src/App.tsx
+++ _personalPage/personalpage/src/App.tsx
@@ -1,6 +1,8 @@
 import { gql, useQuery } from "@apollo/client"
-import { useEffect } from "react"
-import { client } from "./lib/apollo"
+// import { useEffect } from "react"
+// import { client } from "./lib/apollo"
+
+import { Envelope, GithubLogo, LinkedinLogo, UserCircle } from "phosphor-react";
 
 const QUERY = gql`
   query MyQuery {
@@ -9,6 +11,7 @@ const QUERY = gql`
     linkedin
     id
     eMail
+    gitHub
   }
 }
 `
@@ -17,26 +20,57 @@ interface Pessoal {
   nome: string,
   id: string,
   linkedin: string,
-  eMail: string
+  eMail: string,
+  gitHub: string
 }
 
 function App() {
 
   const { data } = useQuery<{ pessoals: Pessoal[] }>(QUERY)
 
-  // console.log(data?.pessoals[0].nome)
+  // console.log(data?.pessoals[0].nome) 
+
+  const linkedin = "https://www.linkedin.com/in/"
 
   return (
     <div>
-      <h1>Algum texto para o Projeto</h1>
+      <h1 className="font-thin text-5xl m-8 text-center">Página Pessoal</h1>
 
-      {data?.pessoals.map( pessoal => (
-        <ul key={pessoal.id}>
-          <li>Nome: {pessoal.nome}</li>
-          <li>Linkedin: {pessoal.linkedin}</li>
-          <li>E-mail: {pessoal.eMail}</li>
+      {data?.pessoals.map(pessoal => (
+        <ul className="mx-8 gap-4 border-b-2 pb-4" key={pessoal.id}>
+          <li className="flex items-center">
+            <UserCircle
+              size={40}
+              weight="fill"
+              className="mr-2"
+            />
+            {pessoal.nome}
+          </li>
+
+          <li className="flex items-center">
+            <Envelope
+              size={40}
+              weight="fill"
+              className="mr-2" />
+            {pessoal.eMail}
+          </li>
+
+          <li className="flex items-center">
+            <a href={"https://www.linkedin.com/in/" + pessoal.linkedin} target="_blank">
+              <LinkedinLogo size={40} weight="fill" />
+            </a>
+
+            <a href={"https://github.com/" + pessoal.gitHub} target="_blank">
+              <GithubLogo size={40} weight="fill" />
+            </a>
+          </li>
         </ul>
       ))}
+
+      {/* <span className="flex">
+        <LinkedinLogo size={40} weight="fill" />
+        {data?.pessoals[0].linkedin}
+      </span> */}
 
     </div>
   )
